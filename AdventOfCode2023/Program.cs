7cb9cd9396228a7bbde32ba3664eafa40607500a
CookieCherith0969 <cookiecherith@gmail.com>
2023-12-01 16:50:26 +1100

    initial commit + puzzle 1

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Puzzle 1:");
+Console.WriteLine(Puzzle1.Calculate().ToString());
+Console.ReadLine();
---
+++ AdventOfCode2023/Puzzle1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Puzzle1
+{
+    public static int Calculate()
+    {
+        int sum = 0;
+        
+
+
+        using (StreamReader sr = new StreamReader(@"C:\Users\cooki\source\repos\AdventOfCode2023\AdventOfCode2023\Inputs\puzzle1.txt"))
+        {
+            string? line = "";
+            bool hasSeenFirstDigit = false;
+            char lastDigit = '0';
+
+            line = sr.ReadLine();
+            while (line != null)
+            {
+
+                foreach(char c in line)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        if (!hasSeenFirstDigit)
+                        {
+                            sum += (int)char.GetNumericValue(c) * 10;
+                            hasSeenFirstDigit = true;
+                        }
+                        lastDigit = c;
+                    }
+                }
+
+                sum += (int)char.GetNumericValue(lastDigit);
+
+                line = sr.ReadLine();
+            }
+        }
+
+        return sum;
+    }
+    
+}
