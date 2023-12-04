1b1cf68657afb9d31f3bbbd6fa410a2b6888d5a5
CookieCherith0969 <cookiecherith@gmail.com>
2023-12-04 18:02:46 +1100

    Day 4 Part 2 Also fixed Part 1, it wasn't counting the last card, and it was unnecessarily figuring out the card ID

--- AdventOfCode2023/Puzzle4.cs
+++ AdventOfCode2023/Puzzle4.cs
@@ -20,26 +20,14 @@ public class Puzzle4
         HashSet<int> ownedNums = new HashSet<int>();
         bool seenID = false;
         bool seenWinning = false;
-        int currentID = 0;
 
         for (int y = 0; y < cards.Length; y++)
         {
-            if (winningNums.Count > 0)
-            {
-                ownedNums.Add(currentNum);
-                int wins = winningNums.Intersect<int>(ownedNums).Count();
-                if(wins > 0)
-                {
-                    sum += (int)Math.Pow(2, wins - 1);
-                }
-            }
-
             currentNum = 0;
             winningNums.Clear();
             ownedNums.Clear();
             seenID = false;
             seenWinning = false;
-            currentID = 0;
 
             string card = cards[y];
             if (card.Length == 0) break;
@@ -49,15 +37,8 @@ public class Puzzle4
                 char c = card[x];
                 if (!seenID)
                 {
-                    if (char.IsDigit(c))
+                    if(c == idChar)
                     {
-                        currentNum *= 10;
-                        currentNum += (int)char.GetNumericValue(c);
-                    }
-                    else if(c == idChar)
-                    {
-                        currentID = currentNum;
-                        currentNum = 0;
                         seenID = true;
                     }
                 }
@@ -92,6 +73,12 @@ public class Puzzle4
                     }
                 }
             }
+            ownedNums.Add(currentNum);
+            int wins = winningNums.Intersect<int>(ownedNums).Count();
+            if (wins > 0)
+            {
+                sum += (int)Math.Pow(2, wins - 1);
+            }
         }
 
 
@@ -100,7 +87,93 @@ public class Puzzle4
 
     public static int CalculateTwo()
     {
-        return -1;
+        int sum = 0;
+        string[] cards = File.ReadAllLines(@"./Inputs/puzzle4.txt");
+
+        int currentNum = 0;
+        HashSet<int> winningNums = new HashSet<int>();
+        HashSet<int> ownedNums = new HashSet<int>();
+        int[] cardNums = new int[cards.Length];
+        for(int i = 0; i < cardNums.Length; i++)
+        {
+            cardNums[i] = 1;
+        }
+        bool seenID = false;
+        bool seenWinning = false;
+        int currentID = 0;
+
+        for (int y = 0; y < cards.Length; y++)
+        {
+            currentNum = 0;
+            winningNums.Clear();
+            ownedNums.Clear();
+            seenID = false;
+            seenWinning = false;
+            currentID = 0;
+
+            string card = cards[y];
+            if (card.Length == 0) break;
+
+            for (int x = 0; x < card.Length; x++)
+            {
+                char c = card[x];
+                if (!seenID)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        currentNum *= 10;
+                        currentNum += (int)char.GetNumericValue(c);
+                    }
+                    else if (c == idChar)
+                    {
+                        currentID = currentNum;
+                        currentNum = 0;
+                        seenID = true;
+                    }
+                }
+                else if (!seenWinning)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        currentNum *= 10;
+                        currentNum += (int)char.GetNumericValue(c);
+                    }
+                    else if (currentNum > 0)
+                    {
+                        winningNums.Add(currentNum);
+                        currentNum = 0;
+                    }
+                    if (c == winningChar)
+                    {
+                        seenWinning = true;
+                    }
+                }
+                else
+                {
+                    if (char.IsDigit(c))
+                    {
+                        currentNum *= 10;
+                        currentNum += (int)char.GetNumericValue(c);
+                    }
+                    else if (currentNum > 0)
+                    {
+                        ownedNums.Add(currentNum);
+                        currentNum = 0;
+                    }
+                }
+            }
+            ownedNums.Add(currentNum);
+            int wins = winningNums.Intersect<int>(ownedNums).Count();
+            int currentCardNum = cardNums[currentID - 1];
+            for (int i = 0; i < wins; i++)
+            {
+                cardNums[currentID + i] += currentCardNum;
+            }
+            sum += currentCardNum;
+        }
+
+
+        return sum;
     }
 
 }
