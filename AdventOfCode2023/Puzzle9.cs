8c2964f239351d949b31951bc4ccd63db15d95a9
CookieCherith0969 <cookiecherith@gmail.com>
2023-12-09 16:46:22 +1100

    Day 9 Part 2

--- AdventOfCode2023/Puzzle9.cs
+++ AdventOfCode2023/Puzzle9.cs
@@ -6,13 +6,6 @@ using System.Threading.Tasks;
 
 public class Puzzle9
 {
-    private readonly record struct Node();
-
-    private static int CompareCyclesByLength()
-    {
-        return -1;
-    }
-
     public static int CalculateOne()
     {
         string[] lines = File.ReadAllLines(@"./Inputs/puzzle9.txt");
@@ -59,15 +52,56 @@ public class Puzzle9
             lastColumn.Clear();
         }
 
-
-
-
         return sum;
     }
 
     public static long CalculateTwo()
     {
-        return -1;
+        string[] lines = File.ReadAllLines(@"./Inputs/puzzle9.txt");
+
+        int sum = 0;
+
+        List<int> currentRow = new List<int>();
+        List<int> firstColumn = new List<int>();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+
+            string[] data = line.Split(' ');
+
+            for (int d = 0; d < data.Length; d++)
+            {
+                currentRow.Add(int.Parse(data[d]));
+            }
+
+            int depth = 0;
+            while (currentRow.Any((num) => num != 0))
+            {
+                depth++;
+                List<int> nextRow = new List<int>();
+                for (int index = 0; index < currentRow.Count - 1; index++)
+                {
+                    int directAbove = currentRow[index];
+                    int nextAbove = currentRow[index + 1];
+                    nextRow.Add(nextAbove - directAbove);
+                }
+                firstColumn.Add(currentRow[0]);
+                currentRow = nextRow;
+            }
+
+            int currentValue = 0;
+            for (; depth > 0; depth--)
+            {
+                currentValue = firstColumn[depth - 1] - currentValue;
+            }
+            sum += currentValue;
+
+            currentRow.Clear();
+            firstColumn.Clear();
+        }
+
+        return sum;
     }
 
 }
