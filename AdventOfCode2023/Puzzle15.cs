37f0c6757cfcc727d2b245b460982e6e72fbbc33
CookieCherith0969 <cookiecherith@gmail.com>
2023-12-16 01:59:55 +1100

    Day 15 Part 2

--- AdventOfCode2023/Puzzle15.cs
+++ AdventOfCode2023/Puzzle15.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 public class Puzzle15
 {
+    private readonly record struct Lens(string Label, int FocalLength);
+
     private const int modValue = 256;
     private const int multValue = 17;
+
+    private const char removeChar = '-';
+    private const char assignChar = '=';
 
     private static int[] multiples = new int[modValue];
 
@@ -16,10 +21,16 @@ public class Puzzle15
         int hash = 0;
         foreach(char c in toHash)
         {
-            hash += (int)c;
-            hash %= modValue;
-            hash = multiples[hash];
+            hash = HashStep(hash, c);
         }
+        return hash;
+    }
+
+    private static int HashStep(int hash, char toHash)
+    {
+        hash += (int)toHash;
+        hash %= modValue;
+        hash = multiples[hash];
         return hash;
     }
 
@@ -29,6 +40,22 @@ public class Puzzle15
         {
             multiples[i] = (i * multValue) % modValue;
         }
+    }
+
+    private static int BoxFocusingPower(List<Lens> box, int boxIndex)
+    {
+        int total = 0;
+        for(int i = 0; i < box.Count; i++)
+        {
+            int subtotal = 0;
+            Lens currentLens = box[i];
+
+            subtotal += boxIndex + 1;
+            subtotal *= i + 1;
+            subtotal *= currentLens.FocalLength;
+            total += subtotal;
+        }
+        return total;
     }
 
     public static int CalculateOne()
@@ -47,7 +74,61 @@ public class Puzzle15
 
     public static long CalculateTwo()
     {
-        return -1;
+        string[] lines = File.ReadAllLines(@"./Inputs/puzzle15.txt");
+        List<Lens>[] boxes = new List<Lens>[modValue];
+        for(int i = 0; i < boxes.Length; i++)
+        {
+            boxes[i] = new List<Lens>();
+        } 
+
+        PopulateMultiples();
+        string[] instructions = lines[0].Split(',');
+
+        foreach (string instruction in instructions)
+        {
+            string label = string.Empty;
+            int boxID = 0;
+            int focalLength = -1;
+            int existingIndex = -1;
+            for(int i = 0; i < instruction.Length; i++)
+            {
+                char c = instruction[i];
+                
+                if(c == removeChar || c == assignChar)
+                {
+                    label = instruction.Substring(0, i);
+                    boxID = Hasher(label);
+
+                    existingIndex = boxes[boxID].FindIndex((l) => l.Label == label);
+                    if(existingIndex >= 0)
+                    {
+                        boxes[boxID].RemoveAt(existingIndex);
+                    }
+                }
+
+                if (char.IsDigit(c))
+                {
+                    focalLength = (int)char.GetNumericValue(c);
+                }
+            }
+
+            if( focalLength > -1)
+            {
+                if(existingIndex < 0)
+                {
+                    existingIndex = boxes[boxID].Count;
+                }
+                boxes[boxID].Insert(existingIndex, new Lens(label, focalLength));
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < boxes.Length; i++)
+        {
+            sum += BoxFocusingPower(boxes[i], i);
+        }
+
+        return sum;
     }
 
 }
